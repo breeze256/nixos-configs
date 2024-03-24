9dd52dc827f236b8b050293268319e1bb1e85a05
libshell64 <18980896953@163.com>
2024-03-24 14:56:11 +0800

    modified:   flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "Starry Night's NixOS Flake";
 
   nixConfig = {
-
     extra-substituters = [
       # nix community's cache server.
       "https://nix-community.cachix.org"
@@ -12,11 +11,9 @@
       # nix community's cache server public key.
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
-
   };
 
   inputs = {
-
     # NixOS official software mirror (tsinghua mirror).
     nixpkgs.url = "git+https://mirrors.tuna.tsinghua.edu.cn/git/nixpkgs?ref=nixos-unstable";
 
@@ -25,15 +22,11 @@
       url = "git+https://mirror.ghproxy.com/https://github.com/nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
   };
 
   outputs = inputs@{ nixpkgs, home-manager, ... }: {
-
     nixosConfigurations = {
-
       MINIPC-PN64 = nixpkgs.lib.nixosSystem {
-
         system = "x86_64-linux";
 
         modules = [
@@ -57,10 +50,8 @@
           }
 
         ];
-
+        
       };
-
     };
-
   };
 }
